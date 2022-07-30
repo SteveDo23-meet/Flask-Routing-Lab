ea629f55e6fcff77854f641e8b1247855928354e
SteveDo23-meet <109904642+SteveDo23-meet@users.noreply.github.com>
2022-07-30 17:20:45 +0300

    if i got more time it would be prettier

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -8,6 +8,30 @@ app = Flask(  # Create a flask app
 )
 
 # Your code should be below
+@app.route('/')
+def idk():
+    return render_template("home.html")
+
+@app.route('/product')
+def idok():
+    return render_template("product.html")
+
+@app.route('/dior_low')
+def dior():
+    return render_template("dior1.html")
+
+@app.route('/chicago')
+def chicago():
+    return render_template("Trophy.html")
+
+@app.route('/cart')
+def cart():
+    return render_template("cart.html")
+
+@app.route('/checkout')
+def pay():
+    return render_template("checkout.html")
+
 
 
 
